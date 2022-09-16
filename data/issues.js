ddd7c4f5ad8ae000ab822d2c79087bc492223ac0
Nick Colley <nick.colley@hippodigital.co.uk>
2022-09-16 19:04:56 +0100

    Cache for local dev

--- data/issues.js
+++ data/issues.js
@@ -1,3 +1,5 @@
+const { join } = require("path");
+const { access, writeFile } = require("fs/promises");
 const { Octokit } = require("@octokit/rest");
 
 const {
@@ -10,29 +12,38 @@ const octokit = new Octokit({
   auth: GITHUB_TOKEN,
 });
 
-module.exports = async function () {
+async function getIssues() {
+  const cachedIssuesPath = join(__dirname, "..", ".cached-issues.json");
   try {
-    const issuesResponse = await octokit.issues.listForRepo({
+    await access(cachedIssuesPath);
+    console.log(`Using cached issues...`);
+    return require(cachedIssuesPath);
+  } catch (error) {
+    console.log(`Looking for issues...`);
+    const { data } = await octokit.issues.listForRepo({
       owner: GITHUB_REPOSITORY_OWNER,
       repo: GITHUB_REPOSITORY_NAME,
     });
-    const { data: issues } = issuesResponse;
-
-    return issues
-      .filter((issue) => {
-        // You can remove this is you want issues from everyone to be rendered.
-        return issue.author_association === "OWNER";
-      })
-      .map((issue) => {
-        const id = parseInt(issue.html_url.split("/").at(-1), 10);
-
-        return {
-          path: `/issues/${id}`,
-          title: issue.title,
-          body: issue.body,
-        };
-      });
-  } catch (error) {
-    return console.error(error);
+    // Cache posts locally...
+    await writeFile(cachedIssuesPath, JSON.stringify(data, null, 2));
+    return data;
   }
+}
+
+module.exports = async function () {
+  const issues = await getIssues();
+  return issues
+    .filter((issue) => {
+      // You can remove this is you want issues from everyone to be rendered.
+      return issue.author_association === "OWNER";
+    })
+    .map((issue) => {
+      const id = parseInt(issue.html_url.split("/").at(-1), 10);
+
+      return {
+        path: `/issues/${id}`,
+        title: issue.title,
+        body: issue.body,
+      };
+    });
 };
