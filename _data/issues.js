0cd8829d55bde9c77a7e518a9d963630db99bc8e
Nick Colley <nick.colley@hippodigital.co.uk>
2022-09-16 17:19:05 +0100

    Use action provided env

--- _data/issues.js
+++ _data/issues.js
@@ -1,36 +1,42 @@
 require("dotenv").config();
 const { Octokit } = require("@octokit/rest");
 
-const { GITHUB_TOKEN, GITHUB_OWNER, GITHUB_REPO } = process.env;
+const { GITHUB_TOKEN, GITHUB_REPOSITORY } = process.env;
 
 const octokit = new Octokit({
   auth: GITHUB_TOKEN,
 });
 
+const [owner, repoName] = GITHUB_REPOSITORY.split("/");
+
 module.exports = async function () {
   try {
     const issuesResponse = await octokit.issues.listForRepo({
-      owner: GITHUB_OWNER,
-      repo: GITHUB_REPO,
+      owner: owner,
+      repo: repoName,
     });
     const { data: issues } = issuesResponse;
 
-    return (
-        issues
-            .filter(issue => {
-                // You can remove this is you want issues from everyone to be rendered.
-                return issue.author_association === 'OWNER';
-            })
-            .map(issue => {
-                // Is there a better way to do this?
-                const id = parseInt(issue.html_url.replace(`https://github.com/${GITHUB_OWNER}/${GITHUB_REPO}/issues/`, ""), 10);
-                return {
-                    slug: id,
-                    title: issue.title,
-                    body: issue.body,
-                };
-            })
-    );
+    return issues
+      .filter((issue) => {
+        // You can remove this is you want issues from everyone to be rendered.
+        return issue.author_association === "OWNER";
+      })
+      .map((issue) => {
+        // Is there a better way to do this?
+        const id = parseInt(
+          issue.html_url.replace(
+            `https://github.com/${GITHUB_REPOSITORY}/issues/`,
+            ""
+          ),
+          10
+        );
+        return {
+          slug: id,
+          title: issue.title,
+          body: issue.body,
+        };
+      });
   } catch (error) {
     return console.error(error);
   }
